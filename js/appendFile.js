0d7c2955f70038815b16b369f4105553793a59ed
MaciejPowallo <maciej.powallo@10g.pl>
2017-09-19 01:42:51 +0200

    update 2.2.0

---
+++ js/appendFile.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+  $(function(){
+    $("#skills").load("skills.html"); 
+  });
+
+  $(function(){
+    $("#portfolio").load("portfolio.html"); 
+  });  
+
+  $(function(){
+    $("#modals").load("modals.html"); 
+  });    
+});
